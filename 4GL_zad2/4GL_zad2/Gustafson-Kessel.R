f9560b0e3b78a4e662ad7d3d144e34289b40dca6
Konrad Slaby <slaby.konrad@gmail.com>
2018-12-09 01:14:43 +0100

    gk algorithm

--- 4GL_zad2/4GL_zad2/Gustafson-Kessel.R
+++ 4GL_zad2/4GL_zad2/Gustafson-Kessel.R
@@ -6,15 +6,21 @@ values = WholesaleCustomersData[, -1]
 data <- na.omit(values) # Remove missing values (NA)
 data <- scale(data) # Scale variables
 
+library(ppclust)
 library(ggplot2)
 library(factoextra)
 library(e1071)
 
-cm <- cmeans(data, centers = 6, iter.max = 400, dist = "euclidean", method = "cmeans", m=2)
-#plot(data, col = cm$cluster)
-fviz_cluster(list(data = data, cluster = cm$cluster),repel = FALSE,
+# Initialize the prototype matrix using K-means++
+v <- inaparc::kmpp(data, k = 6)$v
+# Initialize the memberships degrees matrix 
+u <- inaparc::imembrand(nrow(data), k = 6)$u
+
+# Run FCM with the initial prototypes and memberships
+gk <- gk(data, centers = v, memberships = u, iter.max=50, m = 2)
+
+fviz_cluster(list(data = data, cluster = gk$cluster),repel = FALSE,
              geom = "point", show.clust.cent = TRUE, ellipse.type = "norm",
              ggtheme = theme_minimal(), 
-             main = 'Zbiór danych zawiera 3 ró¿ne grupy,centers=6,\n
-             iter.max=400,dist="euclidean",m=2', 
+             main = 'Zbiór danych zawiera 3 ró¿ne grupy,centers=6,iter.max=50,m=2', 
              alpha = 0.2)
